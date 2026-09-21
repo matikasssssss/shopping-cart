3ddfbc7491138d4134ae4327c486602d40c44c69
matikassssss <matias.farias@ug.uchile.cl>
2026-09-21 03:13:31 -0300

    feat: add context feature

---
+++ src/context/filters.jsx
@@ -0,0 +1,17 @@
+import { createContext, useState } from 'react'
+
+// eslint-disable-next-line react-refresh/only-export-components
+export const FiltersContext = createContext()
+
+export function FiltersProvider({ children }) {
+  const [filters, setFilters] = useState({
+    category: 'all',
+    minPrice: 0,
+  })
+
+  return (
+    <FiltersContext.Provider value={{ filters, setFilters }}>
+      {children}
+    </FiltersContext.Provider>
+  )
+}
